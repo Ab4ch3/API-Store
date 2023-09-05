ecf3e065d2f92cc45d22c65e1d83c2705e461300
miguel <miiguel.abache@gmail.com>
2023-09-05 02:42:55 -0400

    descomentando

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Enable Server Listen
 app.listen(config.PORT, () => {
   logger(`*** SERVER_LISTENING_ON_PORT http://localhost:${config.PORT} ***`);
-  // console.log(
-  //   `*** SERVER_LISTENING_ON_PORT http://localhost:${config.PORT} ***`
-  // );
+  console.log(
+    `*** SERVER_LISTENING_ON_PORT http://localhost:${config.PORT} ***`
+  );
 });
